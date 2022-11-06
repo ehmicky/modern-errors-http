5d12e0723bbcc45b7aee322914b1604424be1c9e
ehmicky <ehmicky@gmail.com>
2022-11-06 23:52:20 +0100

    Decrease package size

--- src/options.js
+++ src/options.js
@@ -1,5 +1,4 @@
 import isPlainObj from 'is-plain-obj'
-import statuses from 'statuses'
 
 // Normalize and validate options
 export const getOptions = function (options = {}) {
@@ -34,12 +33,21 @@ const validateStatus = function (optValue, optName) {
     throw new TypeError(`"${optName}" must be an integer: ${optValue}`)
   }
 
-  if (!STATUSES.has(optValue)) {
+  if (!HTTP_STATUSES.has(optValue)) {
     throw new TypeError(`"${optName}" must be an HTTP status code: ${optValue}`)
   }
 }
 
-const STATUSES = new Set(statuses.codes)
+// TODO: replace with `statuses` package once they become a pure ES module
+/* eslint-disable no-magic-numbers */
+const HTTP_STATUSES = new Set([
+  100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300,
+  301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407,
+  408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424,
+  425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508,
+  509, 510, 511,
+])
+/* eslint-enable no-magic-numbers */
 
 const validateURI = function (optValue, optName) {
   validateString(optValue, optName)
