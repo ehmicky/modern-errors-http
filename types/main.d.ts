0b459296e09d287fc8565a0a65d4cb14a8c1de4e
ehmicky <ehmicky@gmail.com>
2022-11-20 11:33:59 +0100

    Simplify tests

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,70 +1,7 @@
 import type { Info } from 'modern-errors'
+import type { Options, HttpResponse } from 'error-http-response'
 
-/**
- * Options of `modern-errors-http`
- */
-export interface Options {
-  /**
-   * URI identifying and documenting the error class. Ideally, each error class
-   * [should set one](https://github.com/ehmicky/modern-errors/README.md#plugin-options).
-   *
-   * @default undefined
-   */
-  readonly type?: string
-
-  /**
-   * HTTP status code.
-   *
-   * @default undefined
-   */
-  readonly status?: number
-
-  /**
-   * Error class name.
-   *
-   * @default error.name
-   */
-  readonly title?: string
-
-  /**
-   * Error description.
-   *
-   * @default error.message
-   */
-  readonly detail?: string
-
-  /**
-   * URI identifying the value which errored.
-   *
-   * @default undefined
-   */
-  readonly instance?: string
-
-  /**
-   * Error stack trace. Can be set to an empty string.
-   *
-   * @default error.stack
-   */
-  readonly stack?: string
-
-  /**
-   * Additional information. This is always
-   * [safe to serialize as JSON](https://github.com/ehmicky/safe-json-value).
-   * Can be set to an empty object.
-   *
-   * @default any additional `error` properties
-   */
-  readonly extra?: object
-}
-
-/**
- * `BaseError.httpResponse(error)`'s return value
- */
-export interface HttpResponse extends Options {
-  readonly title: string
-  readonly detail: string
-  readonly stack: string
-}
+export type { Options, HttpResponse }
 
 /**
  * `modern-errors-http` plugin
--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -30,30 +30,6 @@ expectError(
 expectError(BaseError.httpResponse(error, { unknown: true }))
 expectNotAssignable<Options>({ unknown: true })
 
-ModernError.subclass('TestError', { plugins: [plugin], http: { type: '' } })
-BaseError.httpResponse(error, { type: '' })
-expectAssignable<Options>({ type: '' })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { type: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { type: true }))
-expectNotAssignable<Options>({ type: true })
-
-ModernError.subclass('TestError', { plugins: [plugin], http: { status: 200 } })
-BaseError.httpResponse(error, { status: 200 })
-expectAssignable<Options>({ status: 200 })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { status: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { status: true }))
-expectNotAssignable<Options>({ status: true })
-
 ModernError.subclass('TestError', { plugins: [plugin], http: { title: '' } })
 BaseError.httpResponse(error, { title: '' })
 expectAssignable<Options>({ title: '' })
@@ -66,69 +42,8 @@ expectError(
 expectError(BaseError.httpResponse(error, { title: true }))
 expectNotAssignable<Options>({ title: true })
 
-ModernError.subclass('TestError', { plugins: [plugin], http: { detail: '' } })
-BaseError.httpResponse(error, { detail: '' })
-expectAssignable<Options>({ detail: '' })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { detail: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { detail: true }))
-expectNotAssignable<Options>({ detail: true })
-
-ModernError.subclass('TestError', { plugins: [plugin], http: { instance: '' } })
-BaseError.httpResponse(error, { instance: '' })
-expectAssignable<Options>({ instance: '' })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { instance: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { instance: true }))
-expectNotAssignable<Options>({ instance: true })
-
-ModernError.subclass('TestError', { plugins: [plugin], http: { stack: '' } })
-BaseError.httpResponse(error, { stack: '' })
-expectAssignable<Options>({ stack: '' })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { stack: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { stack: true }))
-expectNotAssignable<Options>({ stack: true })
-
-ModernError.subclass('TestError', { plugins: [plugin], http: { extra: {} } })
-BaseError.httpResponse(error, { extra: {} })
-expectAssignable<Options>({ extra: {} })
-ModernError.subclass('TestError', {
-  plugins: [plugin],
-  http: { extra: { prop: true } },
-})
-BaseError.httpResponse(error, { extra: { prop: true } })
-expectAssignable<Options>({ extra: { prop: true } })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { extra: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { extra: true }))
-expectNotAssignable<Options>({ extra: true })
-
 expectType<HttpResponse>(httpResponse)
-expectType<string | undefined>(httpResponse.type)
-expectType<number | undefined>(httpResponse.status)
 expectType<string>(httpResponse.title)
-expectType<string>(httpResponse.detail)
-expectType<string | undefined>(httpResponse.instance)
-expectType<string>(httpResponse.stack)
-expectType<object | undefined>(httpResponse.extra)
-expectError(httpResponse.extra?.prop)
 
 expectType<HttpResponse>(error.httpResponse())
 expectError(error.httpResponse(true))
