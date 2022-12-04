e7535096f86aaee5441e380f2a848b99bbbdd1e4
ehmicky <ehmicky@gmail.com>
2022-12-04 20:03:40 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,11 +1,6 @@
 import ModernError from 'modern-errors'
 import plugin, { Options, HttpResponse } from 'modern-errors-http'
-import {
-  expectType,
-  expectAssignable,
-  expectNotAssignable,
-  expectError,
-} from 'tsd'
+import { expectType, expectAssignable, expectNotAssignable } from 'tsd'
 
 const BaseError = ModernError.subclass('BaseError', { plugins: [plugin] })
 const error = new BaseError('')
@@ -14,36 +9,38 @@ const httpResponse = BaseError.httpResponse(error)
 ModernError.subclass('TestError', { plugins: [plugin], http: {} })
 BaseError.httpResponse(error, {})
 expectAssignable<Options>({})
-expectError(BaseError.httpResponse(error, undefined))
+// @ts-expect-error
+BaseError.httpResponse(error, undefined)
 expectNotAssignable<Options>(undefined)
-expectError(
-  ModernError.subclass('TestError', { plugins: [plugin], http: true }),
-)
-expectError(BaseError.httpResponse(error, true))
+// @ts-expect-error
+ModernError.subclass('TestError', { plugins: [plugin], http: true })
+// @ts-expect-error
+BaseError.httpResponse(error, true)
 expectNotAssignable<Options>(true)
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { unknown: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { unknown: true }))
+ModernError.subclass('TestError', {
+  plugins: [plugin],
+  // @ts-expect-error
+  http: { unknown: true },
+})
+// @ts-expect-error
+BaseError.httpResponse(error, { unknown: true })
 expectNotAssignable<Options>({ unknown: true })
 
 ModernError.subclass('TestError', { plugins: [plugin], http: { title: '' } })
 BaseError.httpResponse(error, { title: '' })
 expectAssignable<Options>({ title: '' })
-expectError(
-  ModernError.subclass('TestError', {
-    plugins: [plugin],
-    http: { title: true },
-  }),
-)
-expectError(BaseError.httpResponse(error, { title: true }))
+ModernError.subclass('TestError', {
+  plugins: [plugin],
+  // @ts-expect-error
+  http: { title: true },
+})
+// @ts-expect-error
+BaseError.httpResponse(error, { title: true })
 expectNotAssignable<Options>({ title: true })
 
 expectType<HttpResponse>(httpResponse)
 expectType<string>(httpResponse.title)
 
 expectType<HttpResponse>(error.httpResponse())
-expectError(error.httpResponse(true))
+// @ts-expect-error
+error.httpResponse(true)
