c2a90db37531c1bedbbddf16570282724e4f47eb
ehmicky <ehmicky@gmail.com>
2022-12-13 18:29:14 +0100

    Use arrow functions

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import errorHttpResponse from 'error-http-response'
 
 // Validate options
-const getOptions = function (options) {
+const getOptions = (options) => {
   errorHttpResponse('', options)
   return options
 }
 
 // Turn `error` into a RFC 7807 problem details object
-const httpResponse = function ({ error, options }) {
-  return errorHttpResponse(error, options)
-}
+const httpResponse = ({ error, options }) => errorHttpResponse(error, options)
 
 export default {
   name: 'http',
